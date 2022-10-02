56306f898fb2c2d03140e719e0cc1c4e4d8953a8
piyushsjsu <113589621+piyushsjsu@users.noreply.github.com>
2022-10-02 10:38:44 -0700

    Update AboutActivity.java

--- app/src/main/java/com/example/mamid/studenthelper/AboutActivity.java
+++ app/src/main/java/com/example/mamid/studenthelper/AboutActivity.java
@@ -16,22 +16,18 @@ public class AboutActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
-
-setSupportActionBar(toolbar);
-getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if((Build.VERSION.SDK_INT>=Build.VERSION_CODES.M))
         {
             getWindow().setStatusBarColor(Color.parseColor("#f2f2f2"));
 
-            int flags = toolbar.getSystemUiVisibility();
-            flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            int flags = toolbar.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
             toolbar.setSystemUiVisibility(flags);
         }
     }
